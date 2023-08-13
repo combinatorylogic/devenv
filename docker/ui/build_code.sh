ebcec146bc59a59885fbdcbc8a2208a8b110e496
combinatorylogic <combinatorylogic@users.noreply.github.com>
2023-08-13 14:29:41 +0100

    Allow to define a base for UI layers

--- docker/ui/build_code.sh
+++ docker/ui/build_code.sh
@@ -7,4 +7,12 @@ ncores=$(cat /proc/cpuinfo | grep processor | wc -l)
 
 
 
-docker build . -t ${IMAGE}_code:${VERSION} -f Dockerfile_vscode --cpuset-cpus "0-$ncores" --build-arg baseversion=${VERSION} --build-arg baseimage=base_devenv
+if [ -z ${BASEIMAGE+x} ]; then
+    USEIMAGE=${IMAGE}
+    SUFFIX=code
+else
+    USEIMAGE=${IMAGE}_${BASEIMAGE}
+    SUFFIX=${BASEIMAGE}_code
+fi
+
+docker build . -t ${IMAGE}_${SUFFIX}:${VERSION} -f Dockerfile_vscode --cpuset-cpus "0-$ncores" --build-arg baseversion=${VERSION} --build-arg baseimage=${USEIMAGE}
--- docker/ui/build_emacs.sh
+++ docker/ui/build_emacs.sh
@@ -4,8 +4,19 @@ source ../version.sh
 
 echo "Building ${IMAGE}_emacs:${VERSION}"
 
+
+if [ -z ${BASEIMAGE+x} ]; then
+    USEIMAGE=${IMAGE}
+    SUFFIX=emacs
+else
+    echo "Baseimag"
+    USEIMAGE=${IMAGE}_${BASEIMAGE}
+    SUFFIX=${BASEIMAGE}_emacs
+fi
+
+echo "Image=${USEIMAGE} / ${IMAGE} / ${BASEIMAGE} : ${SUFFIX}"
 ncores=$(cat /proc/cpuinfo | grep processor | wc -l)
 ((ncores--))
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
-docker build . -t ${IMAGE}_emacs:${VERSION} -f Dockerfile_emacs --cpuset-cpus "0-$ncores" --build-arg baseversion=${VERSION} --build-arg baseimage=${IMAGE}
+docker build . -t ${IMAGE}_${SUFFIX}:${VERSION} -f Dockerfile_emacs --cpuset-cpus "0-$ncores" --build-arg baseversion=${VERSION} --build-arg baseimage=${USEIMAGE}
 
